bf586fdd66ad6761ccce1e9a2c4a8df13eea6e58
Marcin <badenski.marcin@gmail.com>
2023-10-22 13:55:43 +0200

    koniec views fumkc, początek class views

--- newspaper/news/urls.py
+++ newspaper/news/urls.py
@@ -1,7 +1,11 @@
 from  . import views
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('articles/', views.list_create_articles, name = 'articles'),
+    path('articles/', views.ListCreateArticles.as_view(), name = 'articles'),
+    path('articles/<int:article_id>/', views.article_detail, name='article_detail'),
     
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,18 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
 from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
 
+class ListCreateArticles(APIView):
+    def get(self, request, format=None):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response({'articles':serializer.data})
+    
+    def post(self, request, format=None):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 @api_view(['GET','POST'])
-def list_create_articles(request):
+def list_create_articles(request, format=None):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse({'articles':serializer.data})
+        return Response({'articles':serializer.data})
     else:
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
@@ -20,3 +35,20 @@ def list_create_articles(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET','PUT','DELETE'])
+def article_detail(request, article_id, format=None):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(instance=article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
