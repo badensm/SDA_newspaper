b328d48e23887fd6c567e644146b838bcad5f1ed
Marcin <badenski.marcin@gmail.com>
2023-10-22 14:25:13 +0200

    class views

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -21,34 +21,27 @@ class ListCreateArticles(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET','POST'])
-def list_create_articles(request, format=None):
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response({'articles':serializer.data})
-    else:
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-@api_view(['GET','PUT','DELETE'])
-def article_detail(request, article_id, format=None):
-    article = get_object_or_404(Article, id=article_id)
-    if request.method == 'GET':
+
+class ArticleDetail(APIView):
+
+    def  get(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
         serializer = ArticleSerializer(article)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-    elif request.method == 'PUT':
+    
+    def put(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
         serializer = ArticleSerializer(instance=article, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
+        
+    def delete(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+  
